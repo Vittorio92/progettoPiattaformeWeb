11480425fc971bdc82ec840dea8ea78c4d653017
Vittorio Gallicchio <gallicchiovittorio01@gmail.com>
2023-08-21 17:38:23 +0200

    Suddivisione in cartelle

---
+++ StoreBackend/src/main/java/com/example/storebackend/Support/Messaggio.java
@@ -0,0 +1,14 @@
+package com.example.storebackend.Support;
+
+public class Messaggio {
+
+    private String messaggio;
+
+    public Messaggio(String m){
+        this.messaggio=m;
+    }
+
+    public String getMessaggio(){
+        return this.messaggio;
+    }
+}
