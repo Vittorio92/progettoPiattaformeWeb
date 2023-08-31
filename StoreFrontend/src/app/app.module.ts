2c81061766758a15d7fab3dc765aa2a2859ff624
Vittorio Gallicchio <gallicchiovittorio01@gmail.com>
2023-08-31 22:36:57 +0200

    Autenticazione

--- StoreFrontend/src/app/app.module.ts
+++ StoreFrontend/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarrelloComponent } from './components/carrello/carrello.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
 @NgModule({
   declarations: [
