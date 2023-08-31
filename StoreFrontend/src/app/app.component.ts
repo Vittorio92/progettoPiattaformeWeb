4cc7920b0627dd9bb55bcd11e039229854f0bae8
Vittorio Gallicchio <gallicchiovittorio01@gmail.com>
2023-09-01 01:02:13 +0200

    Prove frontend con keycloak

--- StoreFrontend/src/app/app.component.ts
+++ StoreFrontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StoreFrontend';
+
+  constructor(private auth: KeycloakService){}
+
+  login(){
+    this.auth.login();
+  }
 }
